df451fd91f7e7b298e75306f43e82226323fc66e
praveen kumar <praveen@textmercato.com>
2021-10-02 07:42:11 +0530

    validation middleware added for global api

--- src/modules/cpbase/controller.ts
+++ src/modules/cpbase/controller.ts
@@ -7,88 +7,82 @@ import { ICpBaseRequest } from "../../models/base-request";
 import fs from 'fs/promises';
 import path from 'path';
 
-const daoService = new DaoService();
-
 export async function cpBaseFunction(req: Request, response: Response) {
     try {
-        let validationErrors = validationResult(req);
-        if(validationErrors.isEmpty()) {
-            let project_id = req.headers['project_auth'];
+    
+        let project_id = req.headers['project_auth'];
 
-            let client = await DbService.getClient();
-            let db = client.db('project-'+project_id);
+        let client = await DbService.getClient();
+        let db = client.db('project-'+project_id);
 
-            const { collectionName, action, data } = req.body as ICpBaseRequest
-            let collection = db.collection(collectionName);
+        const { collectionName, action, data } = req.body as ICpBaseRequest;
+        let collection = db.collection(collectionName);
 
-            let result;
+        let result;
 
-            // find and fineOne  
-            if(action == 'findOne') {
-                console.log('Running findOne operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data);
-            }
-            else if(action == 'find') {
-                console.log('Running findMany operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data).toArray();
-            }
-
-            // insertOne and many
-            else if(action == 'insertOne') {
-                data['_id'] = new ObjectId().toHexString();
-                console.log('Running insertOne operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data);
-            }
-            else if(action == 'insertMany') {
-                data.forEach((record: any) => {
-                    record['_id'] = new ObjectId().toHexString();
-                })
-                console.log('Running insertMany operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data);
-            }
-
-            // update one and many
-            else if(action == 'updateOne') {
-                console.log('Running updateOne operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data.filter, data.update);
-            }
-            else if(action == 'updateMany') {
-                console.log('Running updateMany operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data.filter, data.update);
-            }
-
-            // aggregate
-            else if(action == 'aggregate') {
-                console.log('Running aggregate operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data).toArray();
-            }
-
-            // delete one and many options
-            else if(action == 'deleteOne') {
-                console.log('Running deleteOne operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data);
-            }
-            else if(action == 'deleteMany') {
-                console.log('Running deleteMany operation ...');
-                console.log(JSON.stringify(data, undefined, 3));
-                result = await collection[action](data);
-            }
-
-            client.close();
-            console.log('DB connection closed');
-            response.json({success: true, result});
+        // find and fineOne  
+        if(action == 'findOne') {
+            console.log('Running findOne operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data);
         }
-        else {
-            response.status(400).json({success: false, message: validationErrors});
+        else if(action == 'find') {
+            console.log('Running findMany operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data).toArray();
         }
+
+        // insertOne and many
+        else if(action == 'insertOne') {
+            data['_id'] = new ObjectId().toHexString();
+            console.log('Running insertOne operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data);
+        }
+        else if(action == 'insertMany') {
+            data.forEach((record: any) => {
+                record['_id'] = new ObjectId().toHexString();
+            })
+            console.log('Running insertMany operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data);
+        }
+
+        // update one and many
+        else if(action == 'updateOne') {
+            console.log('Running updateOne operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data.filter, data.update);
+        }
+        else if(action == 'updateMany') {
+            console.log('Running updateMany operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data.filter, data.update);
+        }
+
+        // aggregate
+        else if(action == 'aggregate') {
+            console.log('Running aggregate operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data).toArray();
+        }
+
+        // delete one and many options
+        else if(action == 'deleteOne') {
+            console.log('Running deleteOne operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data);
+        }
+        else if(action == 'deleteMany') {
+            console.log('Running deleteMany operation ...');
+            console.log(JSON.stringify(data, undefined, 3));
+            result = await collection[action](data);
+        }
+
+        client.close();
+        console.log('DB connection closed');
+        response.json({success: true, result});
+        
     }
     catch(err) {
         console.error(err);
--- src/modules/cpbase/routes.ts
+++ src/modules/cpbase/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { multerStorage } from '../../middlewares/multer';
+import { requestValidator } from '../../middlewares/request-validaor';
 import { cpBaseFunction, cpBaseStorage, fetchCollections, fetchDirectories } from './controller';
 import { cpBaseGlobalValidator, fetchCollectionsValidator, fetchDirectoriesValidator } from './validators';
 
 export const cpBaseRoutes = Router();
 
-cpBaseRoutes.post('/global', cpBaseGlobalValidator, cpBaseFunction);
-cpBaseRoutes.get('/collections', fetchCollectionsValidator, fetchCollections)
+cpBaseRoutes.post('/global', cpBaseGlobalValidator, requestValidator, cpBaseFunction);
+cpBaseRoutes.get('/collections', fetchCollectionsValidator, fetchCollections);
 cpBaseRoutes.post('/storage', multerStorage.single('file'), cpBaseStorage);
 cpBaseRoutes.get('/directories', fetchDirectoriesValidator, fetchDirectories);
